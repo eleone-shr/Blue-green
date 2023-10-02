fdf4070f7ba7579fed00b7325b08663c6c4a0a21
Elijah Leone <elijah.leone@shrgroupllc.com>
2023-10-02 14:39:45 -0400

    added PROD deploy to bg

--- BG_deploy.groovy
+++ BG_deploy.groovy
@@ -1,8 +1,9 @@
-def req = ["env":"",
+def pipelineEnv = ["env":"",
                    "changeRequest":""]
 def env = '$(env)'
 def buildID = '$(buildID)'
 def Recipients = "examplename1@exampleemail.com, examplename2@exampleemail.com"
+def goProd = 'false'
 
 def Yamldata = [
     'apiVersion': 'v1',
@@ -28,34 +29,46 @@ node {
         archiveArtifacts artifacts: "manifest.yaml", fingerprint: true
     }
 
-    stage('Env Select') {
-        envTestList = ["DSIT","SITE","PTE","PROD","BLUE-GREEN","END"]
-		
-		req = ""
-		
-        req = input message: "Select an environment to deploy Artifact?",
-            id: 'envResponse',
-                parameters: [string(description: 'Change Request number from KISAM', name: 'changeRequest'),
-                        choice(name: 'env',
-                        choices: envTestList)]
-        println(req)
+    stage('Promotion') {
+        pipelineEnv = ["env":"",
+                      "changeRequest":""]
+        goProd = 'false'
 
-        if (req.env == 'BLUE-GREEN') {
-            echo 'Blue-green deployment selected'
-            iepBlueGreen(req)
+        while(pipelineEnv["env"] != 'PROD' && goProd != 'true') {
+            pipelineEnv = envSelect()
+            if (pipelineEnv["env"] == 'END') {
+                echo 'inner'
+                break;
+            }
+            if (pipelineEnv["env"] == 'BLUE-GREEN'){
+                echo 'Blue-green deployment selected'
+                iepBlueGreen(pipelineEnv)
+            }
+            if (pipelineEnv["env"] == 'DSIT' || pipelineEnv["env"] == 'SAT' || pipelineEnv["env"] == 'SITE' || pipelineEnv["env"] == 'PTE') {
+                echo 'can go to Prod'
+                goProd = 'true'
+            }
+
+            if (pipelineEnv["env"] == 'PROD' && goProd == 'true') {
+                timeout(time: 30, unit: 'MINUTES') {
+                    prodApproval()
+                }
+                echo "Prod deployment approved"
+                stageProd()
+            }
+            if (pipelineEnv["env"] == 'PROD' && goProd != 'true') {
+                prodError()
+            }
         }
+        
     }
 }
 
-def iepBlueGreen(req) {
+def iepBlueGreen(pipelineEnv) {
     stage('Blue-Green Deploy') {
 
-        copyArtifacts(projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER), filter: 'manifest.yaml')
-
-        def file = readYaml file: "manifest.yaml"
-
         response = input message: "Fill out the following for a blue-green manifest.",
-                        id: 'bgResponse',
+                        id: 'bgresponse',
                         parameters: [string(description: 'Current Version in BG', name: 'blueVersion'),
                                     string(description: 'Updating Version', name: 'greenVersion', defaultValue: "2.0"),
                                     string(description: 'Current Build ID in BG', name: 'blueBuildID', defaultValue: "${BUILD_ID}"),
@@ -66,47 +79,154 @@ def iepBlueGreen(req) {
                                     choices: envTestList)]
         println(response)
 
-       
+       bgCreateManifest(response)     
 
-        file['environment'] = response["environment"].toLowerCase()
-        file['version'] = response["blueVersion"] + "," + response["greenVersion"]
-        file['buildID'] = response["blueBuildID"] + "," + response["greenBuildID"]
-        file['kind'] = "promotion"
+    }
 
-        file["deployment"]["type"] = "BG"
-        file["changeRequest"] = req["changeRequest"]
+    stage("BLUE-GREEN Promotion") {
+        bgEnvTestList = envTestList.removeElement("BLUE-GREEN")
 
-        def blueWeight = response["blueWeight"].toInteger()
-        def greenWeight = response["greenWeight"].toInteger()
-        def totalWeight = response["blueWeight"].toInteger() + response["greenWeight"].toInteger()
+        while(response["environment"] != PROD) {
+            bgResponse = input message: "Select next Environment in B-G Deploy",
+                            id: "bgPromotion",
+                            parameters: [string(description: 'Adjust the current Blue Weight', name: 'promoBlueWeight', defaultValue: blueWeight ),
+                                            string(description: 'Adjust the current Green Weight', name: 'promoGreemWeight', defaultValue: greenWeight)
+                                            choice(name: 'ENV',
+                                            choices: bgEnvTestList)]
+            println(bgResponse)
 
-        while (totalWeight != 100) {
-            try { 
-                weightExc = input message: "Please re-input Weights correctly",
-                                  id: "bgWeight",
-                                  parameters: [string(description: 'Blue Weight', name: 'blueWeight'),
-                                               string(description: 'Green Weight', name: 'greenWeight')]
-                blueWeight = weightExc["blueWeight"].toInteger()   
-                greenWeight = weightExc["greenWeight"].toInteger()
-                totalWeight = blueWeight + greenWeight
-            } catch (Exception exc) {
-                echo "Blue green weight not integers!"    
+            bgUpdateManifest(bgResponse)
+
+            if (pipelineEnv["env"] == 'END') {
+                echo 'inner'
+                break;
+            }
+
+            if (pipelineEnv["env"] == 'DSIT' || pipelineEnv["env"] == 'SAT' || pipelineEnv["env"] == 'SITE' || pipelineEnv["env"] == 'PTE') {
+                echo 'can go to Prod'
+                goProd = 'true'
+            }
+
+            if (pipelineEnv["env"] == 'PROD' && goProd == 'true') {
+                timeout(time: 30, unit: 'MINUTES') {
+                    prodApproval()
+                }
+                echo "Prod deployment approved"
+                stageProd()
+            }
+            if (pipelineEnv["env"] == 'PROD' && goProd != 'true') {
+                prodError()
             }
         }
-            file['deployment']["blue"] = ["weight": blueWeight]
-            file['deployment']["green"] = ["weight": greenWeight]
+    }
+}
+
+def bgCreateManifest(response) {
+    copyArtifacts(projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER), filter: 'manifest.yaml')
+
+    def file = readYaml file: "manifest.yaml"
+
+    file['environment'] = response["environment"].toLowerCase()
+    file['version'] = response["blueVersion"] + "," + response["greenVersion"]
+    file['buildID'] = response["blueBuildID"] + "," + response["greenBuildID"]
+    file['kind'] = "promotion"
+
+    file["deployment"]["type"] = "BG"
+    file["changeRequest"] = req["changeRequest"]
+
+    def blueWeight = response["blueWeight"].toInteger()
+    def greenWeight = response["greenWeight"].toInteger()
+    def totalWeight = response["blueWeight"].toInteger() + response["greenWeight"].toInteger()
+
+    while (totalWeight != 100) {
+        try { 
+            weightExc = input message: "Please re-input Weights correctly",
+                                id: "bgWeight",
+                                parameters: [string(description: 'Blue Weight', name: 'blueWeight'),
+                                            string(description: 'Green Weight', name: 'greenWeight')]
+            blueWeight = weightExc["blueWeight"].toInteger()   
+            greenWeight = weightExc["greenWeight"].toInteger()
+            totalWeight = blueWeight + greenWeight
+        } catch (Exception exc) {
+            echo "Blue green weight not integers!"    
+        }
+    }
+        file['deployment']["blue"] = ["weight": blueWeight]
+        file['deployment']["green"] = ["weight": greenWeight]
 
 
-        // sh """
-        //         if [ -f manifest.yaml ] ; then
-        //                 rm -f manifest.yaml
-        //         fi
-        // """
+    // sh """
+    //         if [ -f manifest.yaml ] ; then
+    //                 rm -f manifest.yaml
+    //         fi
+    // """
 
-        writeYaml file: 'manifest.yaml', data: file, overwrite:true
+    writeYaml file: 'manifest.yaml', data: file, overwrite:true
 
 
-        sh 'cat manifest.yaml'
+    sh 'cat manifest.yaml'
+
+    archiveArtifacts artifacts: "manifest.yaml", fingerprint: true
+
+    return blueWeight
+    return greenWeight
+
+}
+
+def bgUpdateManifest(response) {
+    copyArtifacts(projectName: env.JOB_NAME, selector: specific(env.BUILD_NUMBER), filter: 'manifest.yaml')
+
+    def file = readYaml file: "manifest.yaml"
+
+    file['environment'] = response["environment"].toLowerCase()
+
+    file['deployment']["blue"] = ["weight": blueWeight]
+    file['deployment']["green"] = ["weight": greenWeight]
+
+    writeYaml file: 'manifest.yaml', data: file, overwrite:true
+
+
+    sh 'cat manifest.yaml'
+
+    archiveArtifacts artifacts: "manifest.yaml", fingerprint: true
+
+}
+
+def envSelect() {
+    stage('Environment Select') {
+        envTestList = ["DSIT","SAT","SITE","PTE","PROD","BLUE-GREEN","END"]
+            
+        req = ""
+        
+        req = input message: "Select an environment to deploy Artifact?",
+            id: 'envResponse',
+                parameters: [string(description: 'Change Request number from KISAM', name: 'changeRequest'),
+                        choice(name: 'env',
+                        choices: envTestList)]
+        println(req)
+
+        return req
+    }
+}
+
+def prodApproval() {
+    stage("PROD Approval") {
+        def prodApprover = input id: 'iepProdApprover', message: "Approve Deployment to PROD"
+    }
+}
+
+def stageProd() {
+    stage('PROD Deployment') {
+        echo "Welcome to PROD"
+    }
+}
+
+def prodError() {
+    stage('Attempted to go to PROD, need to go to SAT/EITE/SITE first') {
+        catchError(stageResult: 'FAILURE') {
+            sh "exit 1"
+        }
+        echo 'must go to pre-prod first'
     }
 }
                                   
